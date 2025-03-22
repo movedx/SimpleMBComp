65a657d97bf94c65c50313f06c8ec6e4be4e531b
movedx <bgto44@gmail.com>
2025-03-22 07:33:20 +0100

    .

---
+++ Source/Utilities.cpp
@@ -0,0 +1,35 @@
+
+#include "Utilities.h"
+
+
+juce::String getValString(const juce::RangedAudioParameter& param, bool getLow, juce::String suffix)
+{
+    juce::String str;
+
+    auto val = getLow ? param.getNormalisableRange().start : param.getNormalisableRange().end;
+
+    bool useK = truncateKiloValue(val);
+    str << val;
+
+    if (useK)
+    {
+        str << "k";
+    }
+
+    str << suffix;
+
+    return str;
+}
+
+void drawModuleBackground(juce::Graphics& g, juce::Rectangle<int> bounds)
+{
+    using namespace juce;
+    g.setColour(Colours::blueviolet);
+    g.fillAll();
+
+    bounds.reduce(3, 3);
+    g.setColour(Colours::black);
+    g.fillRoundedRectangle(bounds.toFloat(), 3);
+
+    g.drawRect(bounds);
+}
